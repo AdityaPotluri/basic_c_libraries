6c997d55a76ef0afac99ed9994a42538d55558ec
Aditya Potluri <adityapotluri9@Adityas-MacBook-Pro.local>
2022-12-14 23:04:18 -0600

    basic string functions

--- string.c
+++ string.c
@@ -1,14 +1,25 @@
 #include "string.h"
 
+// how does a void pointer function?
+// how do function pointers work?
+int strlen(const char* s) {
+    int i = 0;
+    while(s[i] != '\0') {
+        i++;
+    }
+    return i;
+}
+
+
 void memset(const void *p, const void *s, int n) {
     for(int i = 0; i < n; i++) { 
-        p[i] = s[i];
+        p[i] = s;
     }
 }
 
 void memcpy(const void *p, const void *c, int n) {
     for(int i = 0; i < n; i++) {
-        p[i] = c;
+        p[i] = c[i];
     }
 }
 
@@ -26,4 +37,9 @@ int strcmpn(const char *s1, const char *s2, int n) {
     return a - b;
 }
 
-char* strchr(const char *s, int
+char* strchr(const char *s, const char b) {
+    int i = 0;
+    while(s[i] != NULL && i < strlen(s)) {
+
+    }
+}
